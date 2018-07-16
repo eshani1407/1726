f2ab67c3de39b2e64d8a7bff5428f74d1342fb15
eshani1407 <ekurtarkar@gmail.com>
2018-07-16 15:07:57 +0530

    factorial of 5

---
+++ FACTORIAL_5/src/fact.java
@@ -0,0 +1,16 @@
+
+public class fact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Integer i,fact=1, number=5; //number to calculate factorial    
+		
+		for(i=1;i<=number;i++){    
+			fact=fact*i;    
+		}    
+		
+		System.out.println("Factorial of "+number+" is: "+fact);
+	}
+
+}
