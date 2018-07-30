a52b155b29c556a6b41568550bf3312c7e38df68
eshani1407 <ekurtarkar@gmail.com>
2018-07-30 17:09:55 +0530

    interface and abstract

---
+++ football/src/football/front.java
@@ -0,0 +1,12 @@
+package football;
+
+public class front extends Player{
+
+	@Override
+	public void playerinfo() {
+		// TODO Auto-generated method stub
+		System.out.println("\n\nPlayer id : "+getPlayerId()+ "\n Player name: "+getFname()+" " +getLname());
+		 System.out.print(" position : Front");
+	} 
+
+}
